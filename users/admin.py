1d795986d469c79c42460af5cbd63879769eb9b1
taomee517 <taomee517@qq.com>
2019-06-12 11:29:12 +0800

    将用户注册到admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'date_joined']
+
+
+admin.site.register(User, UserAdmin)
